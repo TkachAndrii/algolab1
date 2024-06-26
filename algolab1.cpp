daac4d25137cb35882335b3192b9bc3f034b264f
TkachAndrii <146113089+TkachAndrii@users.noreply.github.com>
2024-06-26 14:04:11 +0300

    algolab1

--- algolab1.cpp
+++ algolab1.cpp
@@ -3,13 +3,13 @@
 #include <unordered_map>
 #include <cmath>
 #include <limits>
+#include <ctime>
 
 class PerfectHash {
 public:
     PerfectHash(const std::vector<double>& input, int a, int b, int p)
             : a(a), b(b), p(p), emptyMarker(std::numeric_limits<double>::infinity()) {
 
-        // Remove duplicates and initialize main table
         std::unordered_map<double, bool> unique;
         for (double num : input) {
             unique[num] = true;
@@ -17,33 +17,47 @@ public:
 
         mainTable.resize(unique.size());
 
-        // First level hashing
         for (const auto& pair : unique) {
             double num = pair.first;
             int index = hash(num, a, b, mainTable.size());
             mainTable[index].push_back(num);
         }
 
-        // Second level hashing to resolve collisions
         subTables.resize(mainTable.size());
         for (size_t i = 0; i < mainTable.size(); ++i) {
             if (mainTable[i].size() > 1) {
-                handleCollision(i);
+                createSubTable(i);
             } else if (!mainTable[i].empty()) {
                 subTables[i] = { mainTable[i][0] };
             }
         }
     }
 
-    void printHashTable() const {
-        for (size_t i = 0; i < subTables.size(); ++i) {
+    void printMainTableWithCollisions() const {
+        std::cout << "Main hash table (with collisions):\n";
+        for (size_t i = 0; i < mainTable.size(); ++i) {
             std::cout << "Cell " << i << ": ";
-            for (double num : subTables[i]) {
-                if (num != emptyMarker) {
-                    std::cout << num << " ";
-                }
+            for (double num : mainTable[i]) {
+                std::cout << num << " ";
             }
             std::cout << std::endl;
+        }
+    }
+
+    void printSubTables() const {
+        std::cout << "Sub hash tables (size n^2):\n";
+        for (size_t i = 0; i < subTables.size(); ++i) {
+            if (subTables[i].size() > 1) {
+                std::cout << "Cell " << i << ": ";
+                for (double num : subTables[i]) {
+                    if (num != emptyMarker) {
+                        std::cout << num << " ";
+                    } else {
+                        std::cout << "_ ";
+                    }
+                }
+                std::cout << std::endl;
+            }
         }
     }
 
@@ -54,15 +68,19 @@ private:
     double emptyMarker;
 
     int hash(double num, int a, int b, int tableSize) const {
-        return static_cast<int>(std::fmod((a * num + b), p)) % tableSize;
+        int h = static_cast<int>(std::fmod((a * num + b), p));
+        if (h < 0) {
+            h += p;
+        }
+        return h % tableSize;
     }
 
-    void handleCollision(int index) {
+    void createSubTable(int index) {
         int subTableSize = mainTable[index].size() * mainTable[index].size();
         std::vector<double> subTable(subTableSize, emptyMarker);
 
         bool collisionResolved = false;
-        srand(static_cast<unsigned>(time(0)));  // Initialize random seed
+        srand(static_cast<unsigned>(time(0)));  // Ініціалізація випадкового зерна
 
         while (!collisionResolved) {
             subTable.assign(subTableSize, emptyMarker);
@@ -86,10 +104,11 @@ private:
 };
 
 int main() {
-    std::vector<double> input = {1, 2, 3};
-    int a = 1, b = 2, p = 7;
+    std::vector<double> input = {1, 2.2, 3, 4, sqrt(5), 6, -7, 8, 9, 10};
+    int a = 1, b = 2, p = 101;
     PerfectHash perfectHash(input, a, b, p);
     std::cout << "The hash table has been created successfully.\n";
-    perfectHash.printHashTable();
+    perfectHash.printMainTableWithCollisions();
+    perfectHash.printSubTables();
     return 0;
 }
